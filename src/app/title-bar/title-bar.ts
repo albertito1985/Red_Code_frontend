ecc2c429b39067ee5557d1fcbfa9a56993cf2186
Alberto Nuñez <anulo@live.se>
2026-02-18 15:16:06 +0100

    styling details and dark mode switch implemented

--- src/app/title-bar/title-bar.ts
+++ src/app/title-bar/title-bar.ts
@@ -1,38 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBook, faQuoteLeft, faCircleHalfStroke} from '@fortawesome/free-solid-svg-icons';
+import { faBook, faQuoteLeft, faCircleHalfStroke, faEllipsisVertical} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-title-bar',
-  imports: [RouterModule, RouterLinkActive, FormsModule, FontAwesomeModule],
+  imports: [RouterModule, RouterLinkActive, FontAwesomeModule],
   standalone:true,
   templateUrl: './title-bar.html',
   styleUrl: './title-bar.scss'
 })
 export class TitleBarComponent {
   isEnabled: boolean = false; // initial state
+  isMenuOpen: boolean = false;
   faBook = faBook;
   faQuotes = faQuoteLeft;
   faCircleHalfStroke = faCircleHalfStroke;
+  faEllipsisVertical = faEllipsisVertical;
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  setDarkMode(isDarkModeEnabled: boolean): void {
+    this.isEnabled = isDarkModeEnabled;
+    this.onToggle();
+  }
+
+  onThemeSwitchChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.setDarkMode(input.checked);
+  }
+
   onToggle() {
     console.log('Switch state:', this.isEnabled);
     if (this.isEnabled) {
-      this.enableFeature();
+      this.enableDarkMode();
     } else {
-      this.disableFeature();
+      this.disableDarkMode();
     }
   }
 
-  enableFeature() {
-    console.log('Feature enabled');
-    // Add your custom logic here
+  enableDarkMode() {
+    console.log('Dark mode enabled');
+    document.body.setAttribute('data-bs-theme', 'dark');
   }
 
-  disableFeature() {
-    console.log('Feature disabled');
-    // Add your custom logic here
+  disableDarkMode() {
+    console.log('Dark mode disabled');
+    document.body.setAttribute('data-bs-theme', 'light');
   }
 }
